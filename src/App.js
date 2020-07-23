86ac98105cc724190de5bca231585d0ac30e6ded
Abubaker  Shekhani <abubakershekhani@gmail.com>
2020-07-24 00:05:13 +0500

    Add animation to foreground and background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import useWebAnimations from "@wellyshen/use-web-animations";
 import aliceSprite from './images/sprite_running-alice-queen_small.png';
@@ -47,25 +47,69 @@ const App = () => {
       iterations: Infinity
   };
 
-  const background1Movement = useWebAnimations({
+  const background1 = useWebAnimations({
     keyframes: sceneryFrames,
-    timings: sceneryTimingBackground
+    timing: sceneryTimingBackground
   })
 
-  const background2Movement = useWebAnimations({
+  const background2 = useWebAnimations({
     keyframes: sceneryFrames,
-    timings: sceneryTimingBackground
+    timing: sceneryTimingBackground
   })
 
-  const foreground1Movement = useWebAnimations({
+  const foreground1 = useWebAnimations({
     keyframes: sceneryFrames,
-    timings: sceneryTimingForeground
+    timing: sceneryTimingForeground
   })
 
-  const foreground2Movement = useWebAnimations({
+  const foreground2 = useWebAnimations({
     keyframes: sceneryFrames,
-    timings: sceneryTimingForeground
+    timing: sceneryTimingForeground
   })
+
+
+
+  useEffect(() => {
+    let sceneries = [foreground1.getAnimation(), foreground2.getAnimation(), background1.getAnimation(), background2.getAnimation()];
+
+
+    var adjustBackgroundPlayback = function() {
+      if (redQueenAlice.getAnimation().playbackRate < .8) {
+        sceneries.forEach(function(anim) {
+          anim.playbackRate = redQueenAlice.getAnimation().playbackRate/2 * -1;
+        });
+      } else if (redQueenAlice.getAnimation().playbackRate > 1.2) {
+        sceneries.forEach(function(anim) {
+          anim.playbackRate = redQueenAlice.getAnimation().playbackRate/2;
+        });
+      } else {
+        sceneries.forEach(function(anim) {
+          anim.playbackRate = 0;
+        });
+      }
+    }
+    adjustBackgroundPlayback();
+
+    /* If Alice and the Red Queen are running at a speed of 1, the background doesn't move. */
+    /* But if they fall under 1, the background slides backwards */
+    setInterval( function() {
+      /* Set decay */
+      if (redQueenAlice.getAnimation().playbackRate > .4) {
+        redQueenAlice.getAnimation().playbackRate *= .9;
+      }
+      adjustBackgroundPlayback();
+    }, 3000);
+
+    var goFaster = function() {
+      /* But you can speed them up by giving the screen a click or a tap. */
+      redQueenAlice.getAnimation().playbackRate *= 1.1;
+      adjustBackgroundPlayback();
+    }
+
+    document.addEventListener("click", goFaster);
+    document.addEventListener("touchstart", goFaster);
+
+  }, [foreground1, foreground2, background1, background2, redQueenAlice])
 
   return (
 
@@ -77,19 +121,19 @@ const App = () => {
         </div>
       </div>
 
-      <div className="scenery" id="foreground1" ref={foreground1Movement.ref}>
+      <div className="scenery" id="foreground1" ref={foreground1.ref}>
         <img id="palm3" src={palm3}  alt=" " />
       </div>
-      <div className="scenery" id="foreground2" ref={foreground2Movement.ref}>
+      <div className="scenery" id="foreground2" ref={foreground2.ref}>
         <img id="bush" src={bushSmall} alt=" " />
         <img id="w_rook_upright" src={urookupright} alt=" " />
       </div>
-      <div className="scenery" id="background1" ref={background1Movement.ref}>
+      <div className="scenery" id="background1" ref={background1.ref}>
         <img id="r_pawn_upright" src={pawnupright} alt=" " />
         <img id="w_rook" src={urook} alt=" " />
         <img id="palm1" src={palm1}  alt=" " />
       </div>
-      <div className="scenery" id="background2" ref={background2Movement.ref}>
+      <div className="scenery" id="background2" ref={background2.ref}>
         <img id="r_pawn" src={rpawn}  alt=" " />
 
         <img id="r_knight" src={rknight} alt=" " />
